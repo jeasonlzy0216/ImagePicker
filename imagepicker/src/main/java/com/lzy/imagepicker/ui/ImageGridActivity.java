e6793b366500aee185ffd4645816ed141a4e19e6
gaoxx <745570899@qq.com>
2017-02-28 10:47:03 +0800

    1、图片右上角打钩，都会出现闪烁，重新刷新问题

--- imagepicker/src/main/java/com/lzy/imagepicker/ui/ImageGridActivity.java
+++ imagepicker/src/main/java/com/lzy/imagepicker/ui/ImageGridActivity.java
@@ -222,7 +222,7 @@ public class ImageGridActivity extends ImageBaseActivity implements ImageDataSou
             mBtnPre.setEnabled(false);
         }
         mBtnPre.setText(getResources().getString(R.string.preview_count, imagePicker.getSelectImageCount()));
-        mImageGridAdapter.notifyDataSetChanged();
+        
     }
 
     @Override
